06557d46e27d695af0faacd257a6aa602a97f331
marcury122 <mehuljain0503@gmail.com>
2023-12-27 03:07:26 +0530

    updated About me

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("script is running...");
+// console.log("script is running...");
 document.querySelector(".cross").style.display = "none";
 document.querySelector(".hamburger").addEventListener("click", () => {
   document.querySelector(".sidebar").classList.toggle("sidebarGo");
